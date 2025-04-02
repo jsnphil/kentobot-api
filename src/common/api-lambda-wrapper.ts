df0d170abdc45a53886d20a5440ade0a156b9a81
Jason <jsnphil@gmail.com>
2025-04-02 18:42:21 -0500

    Adding lambda wrapper

---
+++ src/common/api-lambda-wrapper.ts
@@ -0,0 +1,34 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { Code } from 'better-status-codes';
+
+export function apiLambdaWrapper<
+  T extends (event: APIGatewayEvent) => Promise<APIGatewayProxyResult>
+>(handle: T) {
+  return async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+    try {
+      const result = await handle(event);
+      return result;
+    } catch (error) {
+      console.error('Error processing request:', error);
+
+      /*
+Sample error handling
+   if (error instanceof SomeCustomError) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: error.message }),
+        };
+      }*/
+
+      return {
+        statusCode: Code.INTERNAL_SERVER_ERROR,
+        body: JSON.stringify({
+          error: {
+            code: 'SystemError',
+            message: 'An error occurred while processing the request'
+          }
+        })
+      };
+    }
+  };
+}
